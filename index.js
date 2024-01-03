63cc8bbb8260c9cd20fce16e071feb723b515bd2
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-02 20:26:30 -0800

    if-else

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ let hamburger = document.getElementById("hamburger");
 let nav = document.querySelector("nav"); 
 hamburger.onclick = function(){
 nav.classList.toggle("open-nav");
+
+if(nav.classList.contains("open-nav")){
+    hamburger.src = "./assets/icon-close.svg";
+}
+
+else{
+    hamburger.src = "./assets/icon-hamburger.svg";
+}
 }
